16ad1e91ce80eede1873829437b7e72d941e65c4
EunKyung Lee <2dmsrud2002@naver.com>
2024-01-15 22:31:36 +0900

    Chore: 백엔드 환경구축

---
+++ src/test/java/com/parkeunyoung/haksugodae/HaksugodaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkeunyoung.haksugodae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaksugodaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
