bd49664077d3c4d6729ac44172906cac23d55d0b
EunKyung Lee <2dmsrud2002@naver.com>
2024-01-17 21:05:29 +0900

    Feat: 이름으로 멤버 찾는 메서드 추가

--- src/main/java/com/parkeunyoung/haksugodae/domain/member/MemberRepository.java
+++ src/main/java/com/parkeunyoung/haksugodae/domain/member/MemberRepository.java
@@ -2,6 +2,9 @@ package com.parkeunyoung.haksugodae.domain.member;
 
 import org.springframework.data.repository.Repository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends Repository<Member, Long> {
     void save(Member member);
+    Optional<Member> findByName(String name);
 }
