2c1c8b72ae8f30a4473d42e2401c9cb597e46e2e
EunKyung Lee <2dmsrud2002@naver.com>
2024-01-22 21:00:05 +0900

    Feat: 종이학 엔티티 파일 생성

---
+++ src/main/java/com/parkeunyoung/haksugodae/domain/crane/Crane.java
@@ -0,0 +1,31 @@
+package com.parkeunyoung.haksugodae.domain.crane;
+
+import com.parkeunyoung.haksugodae.domain.basetime.BaseTimeEntity;
+import com.parkeunyoung.haksugodae.domain.bottle.Bottle;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+class Crane extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long craneId;
+
+    private String title;
+    private String content;
+    private String craneDesign;
+    private String craneColor;
+    private Boolean view;
+
+    @ManyToOne
+    private Bottle bottle;
+
+}
---
+++ src/main/java/com/parkeunyoung/haksugodae/domain/crane/CraneRepository.java
@@ -0,0 +1,6 @@
+package com.parkeunyoung.haksugodae.domain.crane;
+
+import org.springframework.data.repository.Repository;
+
+public interface CraneRepository extends Repository<Crane, Long> {
+}
