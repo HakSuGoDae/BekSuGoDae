5b8844c834431b134f7e18979cfee2a6a9aebefe
EunKyung Lee <2dmsrud2002@naver.com>
2024-02-15 15:18:39 +0900

    Feat: 누구나 접근할 수 있는 종이학 서비스에 페이징 처리를 함

--- src/main/java/com/parkeunyoung/haksugodae/web/controller/CraneController.java
+++ src/main/java/com/parkeunyoung/haksugodae/web/controller/CraneController.java
@@ -3,6 +3,7 @@ package com.parkeunyoung.haksugodae.web.controller;
 import com.parkeunyoung.haksugodae.service.CraneService;
 import com.parkeunyoung.haksugodae.web.dto.CraneDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,15 +26,15 @@ public class CraneController {
     /*
         누구나 종이학 제목과 디자인, craneId 에 대해 반환
      */
-    @GetMapping("/crane")
-    public List<CraneDto.Summary> showCraneByAnyone(@RequestBody CraneDto.Id id) {
-        return craneService.showCraneByAnyone(id);
+    @GetMapping("/crane/{bottleId}/anyone")
+    public List<CraneDto.Summary> showCraneByAnyone(@PathVariable Long bottleId, Pageable pageable) {
+        return craneService.showCraneByAnyone(bottleId, pageable);
     }
 
     /*
         본인의 유리병 일 경우 종이학 내용을 반환
      */
-    @GetMapping("/crane/{bottleId}")
+    @GetMapping("/crane/{bottleId}/owner")
     public List<CraneDto.Detail> showCraneByOwner(@PathVariable Long bottleId, Authentication auth) {
         return craneService.showCraneByOwner(bottleId, auth.getName());
     }
